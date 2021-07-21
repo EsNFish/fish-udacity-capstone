8a97005feb7ef508ae7d142c4ab777df905cc8e0
Brandon Fisher <esnfish@gmail.com>
2021-07-21 17:14:39 -0500

    chore: set up CORS

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask
 
 from .shared_db import setup_db
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 app = Flask(__name__)
 db = setup_db(app)
 
+CORS(app, resources={r"*": {"origins": "*"}})
 migrate = Migrate(app, db)
 
 
